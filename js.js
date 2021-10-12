960d9e52422ed21726cc6f6397920c3a87fcd290
Christian <chris.346@hotmail.com>
2021-10-12 15:54:46 -0400

    first commit

---
+++ js.js
@@ -0,0 +1,36 @@
+
+
+/*
+// add an elemente to the page
+const body = document.body
+body.append("hi" ," bye")  // works for strings
+//appendchild is for elements
+
+//how to create element
+const div = document.createElement('div')
+div.innerText ="divtext"
+div.textContent = "text2 "//overrides top // works even if it is not displayed
+//add to html
+body.append(div)//could be used with appendchild
+
+// render stuff into innerhtml
+div.innerHTML = "<b> hi </b>"; //overrides top
+
+//second way to add stuff to inner html
+const strong = document.createElement("strong")
+strong.innerText ="content";
+body.append(strong);
+
+//varname.remove()
+//document.querySelector("one")
+
+
+//modify actual attributes of an element 
+const variable = document.querySelector('one')
+console.log(variable.getAttribute()) // console.log(variable.title)
+console.log(variable.setAttribute("id", "nameofid"))
+
+//var.removeAttribute("id")
+*/
+
+
