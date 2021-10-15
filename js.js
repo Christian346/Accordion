835450607e1104f23b640f99c13e2d02c343cec1
Christian <chris.346@hotmail.com>
2021-10-15 17:04:32 -0400

    javascript added

--- js.js
+++ js.js
@@ -1,36 +1,21 @@
+const acc = document.querySelectorAll('.item');
 
+for (x of acc){
+    x.addEventListener('click', function () {
+       
+        if (this.classList.contains('dropdown')) 
+        {
+            this.classList.remove('dropdown');
+        }
 
-/*
-// add an elemente to the page
-const body = document.body
-body.append("hi" ," bye")  // works for strings
-//appendchild is for elements
-
-//how to create element
-const div = document.createElement('div')
-div.innerText ="divtext"
-div.textContent = "text2 "//overrides top // works even if it is not displayed
-//add to html
-body.append(div)//could be used with appendchild
-
-// render stuff into innerhtml
-div.innerHTML = "<b> hi </b>"; //overrides top
-
-//second way to add stuff to inner html
-const strong = document.createElement("strong")
-strong.innerText ="content";
-body.append(strong);
-
-//varname.remove()
-//document.querySelector("one")
-
-
-//modify actual attributes of an element 
-const variable = document.querySelector('one')
-console.log(variable.getAttribute()) // console.log(variable.title)
-console.log(variable.setAttribute("id", "nameofid"))
-
-//var.removeAttribute("id")
-*/
-
-
+         else 
+         {
+            for (item of acc) 
+            {
+                item.classList.remove('dropdown');
+            }
+            this.classList.add('dropdown');
+        }
+    })
+}
+// for all items class add event as follows
