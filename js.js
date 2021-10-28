60f9211dcc6419baf32e209125ccd5f368497f86
Christian <chris.346@hotmail.com>
2021-10-28 19:24:28 -0400

    when clicked it gets listed in console

--- js.js
+++ js.js
@@ -16,7 +16,29 @@ for (number of items){
                 number.classList.remove('dropdown');// else remove it from everywhere
             }
             this.classList.add('dropdown'); //if clicked add it 
+
         }
     })
 }
 // for all items class add event as follows
+
+//step 2 make a list 
+//main panel contains all the items 
+//if i select an item of accordion add it to the list 
+
+function listWhenClicked(){
+    var questions = document.querySelectorAll('.question')// gets all questions
+     
+    for(var i=0; i < questions.length; i++){
+        var indv = questions[i]//each individual question
+        
+        indv.addEventListener('click', singleItem)   //to each individual question when clicked call singelitem function
+     }
+}
+
+function singleItem(event){
+    var questionClicked = event.target
+    console.log(questionClicked.innerText)
+}
+
+
