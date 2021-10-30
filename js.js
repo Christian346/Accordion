b13af61f8e2340dc0078156013fe960e9864c917
Christian <chris.346@hotmail.com>
2021-10-30 14:18:44 -0400

    content will be displayed thorugh javascript now

--- js.js
+++ js.js
@@ -1,3 +1,82 @@
+//make the new div
+var contentdiv = document.createElement('div');
+
+//add classes to the new div
+//contentdiv.classList.add('');
+
+
+//find main panel
+var mainpanel = document.querySelectorAll('.main-panel')[0]
+//create variable with content
+
+var content = `
+<div class="item">
+           <div class="upper-acc">
+            <p class="question" >How many team members can I invite?</p> 
+            <img src="images/icon-arrow-down.svg" class ="arrow-down" alt="">
+        </div>
+            <div class="popup">
+            <p> nisi ut aliquip ex ea commodo consequat.</p>
+            </div>
+        </div>
+            <hr>
+            
+            <div class="item">
+            <div class="upper-acc"> 
+            <p class="question" >What is the maximum file upload size?</p>
+            <img src="images/icon-arrow-down.svg" class ="arrow-down" alt="">
+            </div>
+
+            <div class="popup">
+            <p> nisi ut aliquip ex ea commodo consequat.</p>
+            </div>
+        </div>
+            <hr>
+
+            <div class="item">
+            <div class="upper-acc">
+            <p href ="" class="question" >How do I reset my password?</p>
+            <img src="images/icon-arrow-down.svg" class ="arrow-down" alt="">
+            </div>
+
+            <div class="popup">
+            <p> nisi ut aliquip ex ea commodo consequat.</p>
+            </div>
+        </div>
+            <hr>
+
+            <div class="item">
+            <div class="upper-acc">
+            <p href ="" class="question" >Can I cancel my subscription?</p>
+            <img src="images/icon-arrow-down.svg" class ="arrow-down" alt="">
+           </div>
+
+            <div class="popup">
+            <p> nisi ut aliquip ex ea commodo consequat.</p>
+            </div>
+        </div>
+            <hr>
+
+            <div class="item">
+            <div class="upper-acc">
+            <p href ="" class="question" >Do you provide additional support?</p>
+            <img src="images/icon-arrow-down.svg" class ="arrow-down" alt="">
+            </div>
+
+            <div class="popup">
+            <p> nisi ut aliquip ex ea commodo consequat.</p>
+            </div>
+        </div>
+            <hr>
+`
+//add content into contentdiv
+contentdiv.innerHTML = content
+//append the content div to main panel
+ mainpanel.append(contentdiv)
+
+
+ 
+// do the accordion happen
 const items = document.querySelectorAll('.item');
 
 for (number of items){
@@ -27,7 +106,7 @@ for (number of items){
 //if i select an item of accordion add it to the list 
 
 function listWhenClicked(){
-    var questions = document.querySelectorAll('.question')// gets all questions
+    var questions = document.querySelectorAll('.item')// gets all questions
      
     for(var i=0; i < questions.length; i++){
         var indv = questions[i]//each individual question
@@ -42,3 +121,4 @@ function singleItem(event){
 }
 
 
+ 
